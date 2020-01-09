478e5eee53a6cef5dd378b89170fb0baee50fb4a
BryceEvans <evans.bryce@gmail.com>
2020-01-09 12:20:48 -0700

    Here are a few changes.

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-const Photo = (className, data) => {
-    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = new Date(data.date).toLocaleDateString("en-US", options);
+const Photo = (props) => {
+
+    // // I can't get this date to format correctly. I think it might be because it's a string on the server. Does Date() take a string?
+    console.log("DATE: ", props.date)
+    // var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    // const formattedDate = new Date({props.date}).toLocaleDateString("en-US", options);
+    // console.log(formattedDate)
+
+    console.log("PROPS: ", props)
     
     return (
         <div className="card">
-            <h3>{formattedDate}</h3>
-            <a href={data.url}>
-                <img className={className} src={data.url} alt={data.title} />
-            </a>
-            <h2>{data.title}</h2>
-            <p className="copyright"><span className="bold">Copyright: </span>{data.copyright}</p>
-            <p><span className="bold">Explanation: </span>{data.explanation}</p>
+            <h3>{props.date}</h3>
+            <h2>{props.title}</h2>
+            <img src={props.src} alt="someString" />
+            <p className="copyright"><span className="bold">Copyright: </span>{props.copyright}</p>
+            <p><span className="bold">Explanation: </span>{props.explanation}</p>
         </div>
     );
 }
